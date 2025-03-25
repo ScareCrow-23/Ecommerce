1b71231ff5cec7252494f3c79595faa79da12286
Saumil Dhumal <dhumalsaumil@gmail.com>
2025-03-25 21:02:15 +0530

    Frontend Completed

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,12 @@
+import Title from "../components/Title"
+
 const About = () => {
   return (
-    <div>About</div>
+    <div className="">
+      <div className="text-2xl text-center pt-8 border-t ">
+        <Title   text1={"ABOUT"} text2={"US"} />
+      </div>
+    </div>
   )
 }
 
